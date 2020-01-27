2d15a184ff2952cca17dffe79f4e5fe22381b1ff
Ubuntu <ubuntu@ubuntu.attlocal.net>
2020-01-27 20:54:47 +0000

    New ip

--- ip.sh
+++ ip.sh
@@ -1,5 +1,12 @@
-curl https://ipinfo.io/ip > ip.txt
-git add ip.txt
-git commit -m "New ip"
-git push origin2 master
+#!/bin/bash
 
+function myprog() {
+	cd /home/ubuntu/ip
+	curl https://ipinfo.io/ip > ip.txt
+	git add -A
+	git commit -m "New ip"
+	git push origin2 master
+}
+
+
+myprog
